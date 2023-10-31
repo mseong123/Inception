c447c80c645aad671a628baffa2b3d41f30618d6
mseong <mseong123@melee.42.fr>
2023-10-31 08:31:29 +0800

    finally wordpress works after binding volumes

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -3,8 +3,7 @@
 wp config create --dbname=$WP_DB --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=$WP_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-	#wp theme install astra --activate --allow-root
-	#wp plugin update --all --allow-root
+#wp theme install twentynineteen --activate --allow-root
 
 #wp redis enable --allow-root
 
